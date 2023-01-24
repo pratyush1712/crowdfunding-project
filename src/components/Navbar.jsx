892ac76d0af3bc5c7dd1c4c4d040898c9bf4af98
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2023-01-25 00:54:51 +0530

    added proper Link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PersonIcon from "@mui/icons-material/Person";
 import Logo from "../assets/Logo.png";
+import { Link } from "react-router-dom";
 
 export const NavBar = () => {
   const [navbarOpen, setNavbarOpen] = React.useState(false);
@@ -9,32 +10,32 @@ export const NavBar = () => {
       <nav className="relative flex flex-wrap px-2 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="flex items-center text-gray-900 hover:text-gray-900 focus:text-gray-900 mt-2 lg:mt-0">
-            <a href="/">
+            <Link to="/">
               <img
                 src={Logo}
                 className="h-6 sm:h-9 mr-2"
                 alt="Fundstarter"
                 loading="lazy"
               />
-            </a>
-            <a
+            </Link>
+            <Link
               className="text-xs leading-relaxed inline-block font-bold mr-4 py-2 whitespace-nowrap uppercase "
-              href="/explore"
+              to="/explore"
             >
               Explore{" "}
-            </a>
-            <a
+            </Link>
+            <Link
               className="text-xs leading-relaxed font-bold inline-block mr-4 py-2 whitespace-nowrap uppercase "
-              href="/top10"
+              to="/top10"
             >
               Our Top 10 finds{" "}
-            </a>
-            <a
+            </Link>
+            <Link
               className="text-xs font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-              href="/favorites"
+              to="/favorites"
             >
               Team Favorites
-            </a>
+            </Link>
             <button
               className=" cursor-pointer text-xl leading-none px-3 py-2 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
@@ -47,29 +48,29 @@ export const NavBar = () => {
             <div className="container px-4 mx-auto flex flex-wrap items-center justify-between pt-1">
               <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
                 <li className="nav-item">
-                  <a
+                  <Link
                     className="text-xs leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-                    href="/campaign"
+                    to="/campaign"
                   >
                     <span className="ml-2 font-bold">Start a Campaign</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a
+                  <Link
                     className="text-xs leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
-                    href="/about"
+                    to="/about"
                   >
                     <span className="ml-2 font-bold">What We Do</span>
-                  </a>
+                  </Link>
                 </li>
                 <li className="nav-item">
-                  <a
+                  <Link
                     className="text-xs leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase "
-                    href="/profile"
+                    to="/profile"
                   >
                     |&nbsp; <PersonIcon sx={{ mb: 0.001 }} />{" "}
                     <span className="ml-2 font-bold">Profile</span>
-                  </a>
+                  </Link>
                 </li>
               </ul>
             </div>
