11b969c75fc50ecebd381e78f492344960d7e293
Pratyush Sudhakar <pratyushsudhakar03@gmail.com>
2023-02-11 00:07:59 -0500

    made header responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,69 +8,69 @@ export const NavBar = () => {
   return (
     <>
       <nav className="relative flex flex-wrap px-2 mb-3">
-        <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
+        <div className="container md:px-4 mx-auto flex xs:flex-wrap items-center justify-between">
           <div className="flex items-center text-gray-900 hover:text-gray-900 focus:text-gray-900 mt-2 lg:mt-0">
-            <Link to="/">
+            <div to="/" className="md:pb-1 lg:pt-2">
               <img
                 src={Logo}
                 className="h-6 sm:h-9 mr-2"
                 alt="Fundstarter"
                 loading="lazy"
               />
-            </Link>
-            <Link
-              className="text-xs leading-relaxed inline-block font-bold mr-4 py-2 whitespace-nowrap uppercase "
+            </div>
+            <div
+              className="text-xs lg:text-sm leading-relaxed inline-block font-bold md:md:mr-4 sm:mr-1 pb-1 lg:pt-2 whitespace-nowrap uppercase "
               to="/explore"
             >
               Explore{" "}
-            </Link>
-            <Link
-              className="text-xs leading-relaxed font-bold inline-block mr-4 py-2 whitespace-nowrap uppercase "
+            </div>
+            <div
+              className="text-xs lg:text-sm leading-relaxed font-bold inline-block md:mr-4 sm:mr-1 pb-1 lg:pt-2 whitespace-nowrap uppercase "
               to="/top10"
             >
               Our Top 10 finds{" "}
-            </Link>
-            <Link
-              className="text-xs font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+            </div>
+            <div
+              className="text-xs lg:text-sm font-bold leading-relaxed inline-block md:mr-4 sm:mr-1 pb-1 lg:pt-2 whitespace-nowrap uppercase"
               to="/favorites"
             >
               Team Favorites
-            </Link>
-            <button
+            </div>
+            {/* <button
               className=" cursor-pointer text-xl leading-none px-3 py-2 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
-              <i className="fas fa-bars"></i>
-            </button>
+              <i className="fas fa-bars">Lol</i>
+            </button> */}
           </div>
           <div>
-            <div className="container px-4 mx-auto flex flex-wrap items-center justify-between pt-1">
-              <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            <div className="container md:px-4 md:mx-auto flex flex-wrap items-center justify-between pt-1">
+              <ul className="flex xs:flex-row list-none lg:ml-auto">
                 <li className="nav-item">
-                  <Link
-                    className="text-xs leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+                  <div
+                    className="text-xs lg:text-sm leading-relaxed inline-block md:mr-4 sm:mr-1 pt-3.5 whitespace-nowrap uppercase"
                     to="/campaign"
                   >
-                    <span className="ml-2 font-bold">Start a Campaign</span>
-                  </Link>
+                    <span className="md:ml-2 font-bold">Start a Campaign</span>
+                  </div>
                 </li>
                 <li className="nav-item">
-                  <Link
-                    className="text-xs leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase"
+                  <div
+                    className="text-xs lg:text-sm leading-relaxed inline-block md:mr-4 sm:mr-1 pt-3.5 py-2 whitespace-nowrap uppercase"
                     to="/about"
                   >
-                    <span className="ml-2 font-bold">What We Do</span>
-                  </Link>
+                    <span className="md:ml-2 font-bold">What We Do</span>
+                  </div>
                 </li>
                 <li className="nav-item">
-                  <Link
-                    className="text-xs leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase "
+                  <div
+                    className="text-xs lg:text-sm leading-relaxed inline-block md:mr-4 sm:mr-1 pt-2.5 py-2 whitespace-nowrap uppercase "
                     to="/profile"
                   >
                     |&nbsp; <PersonIcon sx={{ mb: 0.001 }} />{" "}
                     <span className="ml-2 font-bold">Profile</span>
-                  </Link>
+                  </div>
                 </li>
               </ul>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}", "./index.html"],
   theme: {
     extend: {},
+    fontSize: {
+      xs: ['9px', '12px'],
+      sm: ['12px', '16px'],
+      base: ['16px', '24px'],
+      lg: ['20px', '28px'],
+      xl: ['24px', '32px'],
+    }
   },
   plugins: [],
 };
